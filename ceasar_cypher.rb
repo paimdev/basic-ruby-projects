c98f898900bf85af6eb99d7e2649adef19db9183
paimdev <bathkelucas@gmail.com>
2021-08-19 13:34:32 -0300

    Add cypher function to upper case characters

--- ceasar_cypher.rb
+++ ceasar_cypher.rb
@@ -2,14 +2,15 @@ def ceasar_cypher(string, rot)
   ord_arr = string.chars.map { |char| char.ord }
   resulting_arr = ord_arr.map do |ord|
     if ord > 97
-        new_position = (ord - 97) + rot
-        ord = ((ord - 97) + rot) % 26
-        ord += 97
-        ord.chr  
+      ord = ((ord - 97) + rot) % 26
+      ord += 97
+      ord.chr  
     else
-      p "hei"
+      ord = ((ord - 65) + rot) % 26
+      ord += 65
+      ord.chr
     end
   end
   resulting_arr.join
 end
-p ceasar_cypher("zzzz", 3)
+p ceasar_cypher("ZZzzzCCC", 7)
