ccf2c7081119c0a3da4d33d9d8f789873626ccee
paimdev <bathkelucas@gmail.com>
2021-08-20 12:51:08 -0300

    Add a counter to function and return the result

--- sub_strings.rb
+++ sub_strings.rb
@@ -2,9 +2,13 @@ def substrings(string_to_find, dictionary)
   selected_strings = dictionary.select do |str| 
     string_to_find.include?(str) || str.include?(string_to_find)
   end
-  p selected_strings
+  string_count = selected_strings.reduce(Hash.new(0)) do |result, string|
+                   result[string] += 1
+                   result
+  end
+  p string_count
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-substrings("i", dictionary)
+substrings("down", dictionary)
