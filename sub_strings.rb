1d24285357db5a010dc40f923a81c292fd04747a
paimdev <bathkelucas@gmail.com>
2021-08-20 14:01:20 -0300

    Fix counter and refactor iteration

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,14 +1,14 @@
 def substrings(string_to_find, dictionary)
-  selected_strings = dictionary.select do |str| 
-    string_to_find.include?(str) || str.include?(string_to_find)
+  case_insensitive_string = string_to_find.downcase
+  result = Hash.new(0)
+  
+  dictionary.each do |word|
+    matches = case_insensitive_string.scan(word).length
+    result[word] = matches unless matches == 0
   end
-  string_count = selected_strings.reduce(Hash.new(0)) do |result, string|
-                   result[string] += 1
-                   result
-  end
-  p string_count
+  return result
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-substrings("down", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
