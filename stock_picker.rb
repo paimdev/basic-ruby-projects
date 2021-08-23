e0489606cbef8f094dc5dead66d40180997ca8eb
paimdev <bathkelucas@gmail.com>
2021-08-23 17:51:57 -0300

    Finish the function stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,5 +1,23 @@
 def stock_picker(days_prices)
-  
+  min_p = days_prices[0]
+  min_i = 0
+
+  profit = 0
+  days = [0, 0]
+
+  days_prices.each_with_index do |p, i|
+    if p < min_p
+      min_p = p
+      min_i = i
+      next
+    end
+
+    if (p - min_p) > profit
+      profit = p - min_p
+      days = [min_i, i]
+    end
+  end
+  days
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
